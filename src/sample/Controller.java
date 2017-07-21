8300b463bb0f5b4cdb9ab85d7520a1a683a9cf4b
Sujay Tadwalkar <sujayt123@gmail.com>
2017-07-20 23:43:33 -0500

    Partial  initialization routine implemented

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    /* Access to the GUI representation of the board. */
+    private StackPane[][] board_cells = new StackPane[15][15];
+
+    /* Access to the GUI GridPane.*/
+    @FXML
+    GridPane gridPane;
+
+    /*
+     * Runs initialization routines right after the view loads.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        gridPane.getChildren().forEach((child)->{
+            if (child instanceof StackPane)
+            {
+                final int row = gridPane.getRowIndex(child);
+                final int col = gridPane.getColumnIndex(child);
+                board_cells[row][col] = (StackPane) child;
+            }
+        });
+    }
 }
